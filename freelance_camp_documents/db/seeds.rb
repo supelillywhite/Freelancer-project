6189a2a03aa0a1e07d52995c2bacae4ed9b4af2a
Jerel Lillywhite <supelillywhite@gmail.com>
2018-02-05 13:59:39 -0700

    Added the back end rails app for documents and set it up for api connection

---
+++ freelance_camp_documents/db/seeds.rb
@@ -0,0 +1,20 @@
+FreelanceDocument.create!(
+  title: "Document 1", 
+  description: "He hasn't got shit all over him. You can't expect to wield supreme power just 'cause some watery tart threw a sword at you! Listen. Strange women lying in ponds distributing swords is no basis for a system of government. Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.", 
+  file_url: "https://drive.google.com/file/d/1zbTbLNdxUyYX0GJbUjAuIR0KC92VMYUa/view?usp=sharing",
+  image_url: "https://thump-images.vice.com/images/articles/meta/2016/06/01/we-asked-the-hippies-of-lightning-in-a-bottle-what-it-means-to-be-a-hippie-1464803352.jpg?crop=0.9219194421506559xw:1xh;center,center&resize=1200:*" 
+  )
+
+FreelanceDocument.create!(
+  title: "Document 2", 
+  description: "Why do you think that she is a witch? The Lady of the Lake, her arm clad in the purest shimmering samite, held aloft Excalibur from the bosom of the water, signifying by divine providence that I, Arthur, was to carry Excalibur. That is why I am your king.", 
+  file_url: "https://drive.google.com/file/d/1zbTbLNdxUyYX0GJbUjAuIR0KC92VMYUa/view?usp=sharing",
+  image_url: "http://cdn.groovyhistory.com/content/34658/9f2faa7bf413a7746b7c2f7438485e6e.jpg" 
+  )
+
+FreelanceDocument.create!(
+  title: "Document 3", 
+  description: "Ni! Ni! Ni! Ni! …Are you suggesting that coconuts migrate? Shh! Knights, I bid you welcome to your new home. Let us ride to Camelot! Oh! Come and see the violence inherent in the system! Help, help, I'm being repressed!", 
+  file_url: "https://drive.google.com/file/d/1zbTbLNdxUyYX0GJbUjAuIR0KC92VMYUa/view?usp=sharing",
+  image_url: "http://www.sikkensprize.org/wp-content/uploads/2017/03/Sikkensprize_HIPPIES-2-1-800x534-c-center.jpg" 
+  )
